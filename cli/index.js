d4753408f852aba441bc7c6dc228a01b16d5620c
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-15 01:22:04 +0100

    chore: bump version

--- cli/index.js
+++ cli/index.js
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name("nexujs-cli")
   .description("CLI for scaffolding and managing NexuJs apps")
-  .version("0.1.15");
+  .version("0.1.16");
 
 program
   .command("update-deps")
