80789ab3bb486c026e1e8a2543c8c21fe7206f0b
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-02-20 15:06:04 +0100

    refactor(cli): remove cli-spinners

--- cli/index.js
+++ cli/index.js
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name("nexujs-cli")
   .description("CLI for scaffolding and managing NexuJs apps")
-  .version("0.1.30");
+  .version("0.1.31");
 
 program
   .command("update-deps")
